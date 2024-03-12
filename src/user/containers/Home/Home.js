b770eb5f32a3db13093f1267134dd2da1622587e
meet3475 <meetdobariya480@gmail.com>
2024-03-12 15:26:56 +0530

    done_home

--- src/user/containers/Home/Home.js
+++ src/user/containers/Home/Home.js
@@ -37,6 +37,38 @@ function Home(props) {
     }
 }
 
+let vegetable_carousel = {
+  autoplay: true,
+  smartSpeed: 1500,
+  center: false,
+  dots: true,
+  loop: true,
+  margin: 25,
+  nav : true,
+  navText : [
+      '<div class="owl-prev"><i class="bi bi-arrow-left"></i></div>',
+      '<div class="owl-next"><i class="bi bi-arrow-right"></i></div>'
+  ],
+  responsiveClass: true,
+  responsive: {
+      0:{
+          items:1
+      },
+      576:{
+          items:1
+      },
+      768:{
+          items:2
+      },
+      992:{
+          items:3
+      },
+      1200:{
+          items:4
+      }
+  }
+}
+
 
     return (
         <div>
@@ -537,7 +569,7 @@ function Home(props) {
   <div className="container-fluid vesitable py-5">
     <div className="container py-5">
       <h1 className="mb-0">Fresh Organic Vegetables</h1>
-      <OwlCarousel className="owl-carousel vegetable-carousel justify-content-center">
+      <OwlCarousel {...vegetable_carousel} className="owl-carousel vegetable-carousel justify-content-center">
         <div className="border border-primary rounded position-relative vesitable-item">
           <div className="vesitable-img">
             <img src="img/vegetable-item-6.jpg" className="img-fluid w-100 rounded-top" alt />
