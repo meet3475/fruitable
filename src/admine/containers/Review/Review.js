b10ce51019e0045a971de954281c26c84bc3972e
meet3475 <meetdobariya480@gmail.com>
2024-03-19 11:22:39 +0530

    done_19

---
+++ src/admine/containers/Review/Review.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Review(props) {
+    return (
+        <div>
+            <h1>Review Page</h1>
+        </div>
+    );
+}
+
+export default Review;
--- src/routes/AdmineRoute/AdmineRoute.js
+++ src/routes/AdmineRoute/AdmineRoute.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Product from '../../admine/containers/Product/Product';
+import Layout from '../../admine/components/Layout/Layout';
+import Review from '../../admine/containers/Review/Review'
 
 function AdmineRoute(props) {
     return (
         <div>
-            <Routes>
-                <Route exact path="/product" element={<Product />} />
-            </Routes>
+            <Layout>
+                <Routes>
+                    <Route exact path="/product" element={<Product />} />
+                    <Route exact path="/review" element={<Review />} />
+                </Routes>
+            </Layout>
         </div>
     );
 }
